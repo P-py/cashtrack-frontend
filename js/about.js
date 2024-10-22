d3d1fb28a4f39b87ce0b4ade598f52bc7959d077
Pedro Salviano <pedrosalviano170@gmail.com>
2024-10-22 17:09:29 -0300

    :sparkles: add final mobile layout for about page

--- js/about.js
+++ js/about.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
     const expandableImages = document.querySelectorAll('.expandableImage');
     const modal = document.getElementById('imageModal');
     const modalImg = document.getElementById('modalImage');
@@ -11,8 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
             modalImg.src = image.src;
             modalImg.alt = image.alt;
 
-            // Verifica se o src da imagem contém "data-diagram.png"
-            if (image.src.includes("data-diagram.png")) {
+            // Verifies if the image is the diagram image
+            // This image has a different height, so we need to adjust the margin-top
+            if (window.innerWidth > 768 && image.src.includes("data-diagram.png")) {
                 modalContent.style.marginTop = '55vh';
             } else {
                 modalContent.style.marginTop = '0';
@@ -20,43 +23,67 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // When the burger button is clicked, activates the hidden nav menu
+    burger.addEventListener('click', () => {
+        console.log("menu clicado caralha")
+        navLinks.classList.toggle('nav-active');
+    });
+
+    // Close the modal when the user clicks on the close button
     closeModal.addEventListener('click', () => {
         modal.style.display = 'none';
     });
 
+    // Close the modal when the user clicks outside the modal
     window.addEventListener('click', (event) => {
         if (event.target == modal) {
             modal.style.display = 'none';
         }
     });
 
-    // Inicializa o Swiper.js para as imagens
-    const swiperImages = new Swiper('.image-content-mobile', {
+    // Swiper.js instance for the image content
+    const swiperImages = new Swiper('.what-we-do-images', {
         loop: true,
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
+        lazyPreloadPrevNext: 0, // Does not render the next and previous slides for data saving
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        slidesPerView: 1,
-        spaceBetween: 10,
+        breakpoints: {
+            // Responsive breakpoints
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
     });
 
-    // Inicializa o Swiper.js para a seção de contato
-    const swiperContact = new Swiper('.contact-container-mobile', {
+    // Swiper.js instance for the contact section
+    const swiperContact = new Swiper('.who-we-are-container-mobile', {
         loop: true,
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
+        lazyPreloadPrevNext: 0, // Does not render the next and previous slides for data saving
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        slidesPerView: 1,
-        spaceBetween: 10,
+        breakpoints: {
+            // Responsive breakpoints
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
     });
 });
