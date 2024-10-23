29fbdf931470573edd6ed0f2ce277dcaf2cccfa0
Pedro Salviano <pedrosalviano170@gmail.com>
2024-10-23 14:23:12 -0300

    :bug: fix nav on login page and add animation on index

--- js/login.js
+++ js/login.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
     const submitButtons = document.querySelectorAll('.submit-btn');
 
+    // When the burger button is clicked, activates the hidden nav menu
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+    });
+    
     // Goes through all submit buttons and adds a loading class to them if clicked
     submitButtons.forEach(button => {
         button.addEventListener('click', (event) => {
