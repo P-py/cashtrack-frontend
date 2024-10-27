1414b2b18a325e5707fd0d79abe9f6712eb1379b
Pedro Salviano <pedrosalviano170@gmail.com>
2024-10-27 06:57:30 -0300

    :sparkles: add main dashboard

---
+++ js/add-new.js
@@ -0,0 +1,134 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const modal = document.getElementById('addModal');
+    const modalTitle = document.getElementById('modalTitle');
+    const incomeForm = document.getElementById('addFormIncome');
+    const expenseForm = document.getElementById('addFormExpense');
+    const incomeLabel = document.getElementById('incomeLabel');
+    const expenseLabel = document.getElementById('expenseLabel');
+    const typeExpense = document.getElementById('type-select-expenses');
+    const typeIncome = document.getElementById('type-select-incomes');
+    const valueIncome = document.getElementById('value-income');
+    const valueExpense = document.getElementById('value-expense')
+    // Message modal for error or success messages
+    const messageModal = document.getElementById('messageModal');
+    const closeModal = modal.querySelector('.close');
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+    const accessToken = getCookie('accessToken');
+
+    //Function to show the add new expense/income modal
+    function showModal(title, type) {
+        if (type === 'expense'){
+            incomeForm.style.display = 'none';
+            expenseForm.style.display = 'flex';
+        } else {
+            incomeForm.style.display = 'flex';
+            expenseForm.style.display = 'none';
+        }
+        modalTitle.textContent = title;
+        modal.style.display = 'flex';
+
+        modal.querySelector('.close').addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        });
+    }
+
+    function showMessage(message) {
+        messageModal.textContent = message;
+        modal.style.display = 'block';
+
+        closeModal.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        });
+    }
+
+    // Adiciona eventos aos botões
+    document.getElementById('add-income-btn').addEventListener('click', () => {
+        showModal('Adicionar Nova Entrada', 'income');
+    });
+
+    document.getElementById('add-expense-btn').addEventListener('click', () => {
+        showModal('Adicionar Nova Despesa', 'expense');
+    });
+
+    document.getElementById('addFormExpense').addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const data = { 
+            expenseLabel: expenseLabel.value,
+            value: parseFloat(valueExpense.value),
+            type: typeExpense.value
+        }
+
+        try {
+            const response = await fetch(`${API_URL}/expenses`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                credentials: 'include',
+                body: JSON.stringify(data)
+            });
+
+            if (response.status === 201) {
+                window.location.reload();
+                showMessage('Adicionado com sucesso!');
+            } else {
+               showMessage('Erro ao adicionar. Tente novamente.');
+            }
+        } catch (error) {
+            console.error('Erro ao enviar a requisição:', error);
+            showMessage('Erro ao adicionar. Tente novamente.');
+        }
+    });
+
+    document.getElementById('addFormIncome').addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const data = {
+            incomeLabel: incomeLabel.value,
+            value: parseFloat(valueIncome.value),
+            type: typeIncome.value
+        }
+
+        try {
+            const response = await fetch(`${API_URL}/incomes`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                credentials: 'include',
+                body: JSON.stringify(data)
+            });
+
+            if (response.status === 201) {
+                showMessage('Adicionado com sucesso!');
+                window.location.reload();
+            } else {
+               showMessage('Erro ao adicionar. Tente novamente.');
+            }
+        } catch (error) {
+            console.error('Erro ao enviar a requisição:', error);
+            showMessage('Erro ao adicionar. Tente novamente.');
+        }
+    });
+});
---
+++ js/dashboard.js
@@ -0,0 +1,247 @@
+const API_URL = 'http://localhost:8080';
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+    function deleteCookie(name) {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    }
+
+    document.getElementById('logout-btn').addEventListener('click', () => {
+        deleteCookie('accessToken');
+        window.location.href = 'index.html';
+    });
+
+    const accessToken = getCookie('accessToken');
+    if (!accessToken) {
+        window.location.href = 'index.html';
+        return;
+    }
+
+    const typeTagMapping = {
+        'SALARY': 'Salário',
+        'EXTRA': 'Extras',
+        'GIFT': 'Presente',
+        'MONTHLY_ESSENTIAL': 'Essencial',
+        'ENTERTAINMENT': 'Lazer',
+        'INVESTMENTS': 'Investimento',
+        'LONGTIME_PURCHASE': 'Parcelamento'
+    }
+
+    // GET request for the user account data
+    try {
+        const accountDataResponse = await fetch(`${API_URL}/users/account`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+        });
+
+        if (accountDataResponse.ok) {
+            const usernameData = await accountDataResponse.json();
+            const usernameElement = document.getElementById('username');
+            if (usernameElement) {
+                usernameElement.textContent = usernameData.username;
+            }
+        } else {
+            console.error('Erro ao obter os dados do usuário');
+        }
+    } catch (error) {
+        console.error('Erro ao enviar a requisição');
+    }
+
+    // GET request for the user's transactions
+    try {
+        const transactionsResponse = await fetch(`${API_URL}/users/balance`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+        });
+
+        if (transactionsResponse.ok) {
+            const transactionsData = await transactionsResponse.json();
+            const expenseElement = document.getElementById('total-expenses');
+            const incomeElement = document.getElementById('total-incomes');
+            const balanceElement = document.getElementById('balance');
+            if (expenseElement && incomeElement) {
+                expenseElement.textContent = `R$ ${transactionsData.totalExpenses.toFixed(2)}`;
+                incomeElement.textContent = `R$ ${transactionsData.totalIncomes.toFixed(2)}`;
+                balanceElement.textContent = `R$ ${transactionsData.balance.toFixed(2)}`;
+            }
+        } else {
+            console.error('Erro ao obter as transações do usuário');
+        }
+    } catch (error) {
+        console.error('Erro ao enviar a requisição');
+    }
+
+    // GET request for the user's income list
+    try {
+        const incomesResponse = await fetch(`${API_URL}/incomes`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+        });
+
+        if (incomesResponse.ok) {
+            const incomesData = await incomesResponse.json();
+            const lastIncomesElement = document.getElementById('last-incomes');
+            if (lastIncomesElement) {
+                const sortedIncomes = incomesData.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+                const lastThreeIncomes = sortedIncomes.slice(0, 3);
+                if (lastThreeIncomes.length === 0) {
+                    lastIncomesElement.innerHTML = `<li style="border: 0px;"><p>Não há entradas registradas</p></li>`
+                } else {
+                    lastIncomesElement.innerHTML = lastThreeIncomes.map(income => `
+                        <hr>
+                        <li>
+                            <h4>${income.incomeLabel}</h4>
+                            <span style="display:none;">${income.id}</span>
+                            <div>
+                                <p><span class="value">R$ ${income.value.toFixed(2)}</span></p>
+                                <p><span class="type-tag">${typeTagMapping[income.type] || income.type}</span></p>
+                                <p>${new Date(income.dateCreated).toLocaleDateString()}</p>
+                                <button class="delete-btn-desktop" id="delete-btn" data-id="income-${income.id}">
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                                <button class="edit-btn-desktop" id="edit-btn" data-id="income-${income.id}">
+                                    <i class="fas fa-pencil-alt"></i>
+                                </button>
+                            </div>
+                            <button class="delete-btn" id="delete-btn" data-id="income-${income.id}">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                            <button class="edit-btn" id="edit-btn" data-id="income-${income.id}">
+                                <i class="fas fa-pencil-alt"></i>
+                            </button>
+                        </li>
+                    `).join('');
+                }
+            }
+        } else {
+            console.error('Erro ao obter as transações do usuário');
+        }
+    } catch (error) {
+        console.error('Erro ao enviar a requisição');
+    }
+
+
+    // GET request for the user's expense list
+    try {
+        const expensesResponse = await fetch(`${API_URL}/expenses`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            },
+        });
+
+        if (expensesResponse.ok) {
+            const expensesData = await expensesResponse.json();
+            const lastExpensesElement = document.getElementById('last-expenses');
+            if (lastExpensesElement) {
+                const sortedExpenses = expensesData.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+                const lastThreeExpenses = sortedExpenses.slice(0, 3);
+                if (lastThreeExpenses.length === 0) {
+                    lastExpensesElement.innerHTML = `<li style="border: 0px;"><p>Não há despesas registradas</p></li>`
+                } else {
+                    lastExpensesElement.innerHTML = lastThreeExpenses.map(expense => `
+                        <hr>
+                        <li>
+                            <h4>${expense.expenseLabel}</h4>
+                            <span style="display:none;">${expense.id}</span>
+                            <div>
+                                <p><span class="value">R$ ${expense.value.toFixed(2)}</span></p>
+                                <p><span class="type-tag">${typeTagMapping[expense.type] || expense.type}</span></p>
+                                <p>${new Date(expense.dateCreated).toLocaleDateString()}</p>
+                                <button class="delete-btn-desktop" id="delete-btn" data-id="expense-${expense.id}">
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                                <button class="edit-btn-desktop" id="edit-btn" data-id="expense-${expense.id}">
+                                    <i class="fas fa-pencil-alt"></i>
+                                </button>
+                            </div>
+                            <button class="delete-btn" id="delete-btn" data-id="expense-${expense.id}">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                            <button class="edit-btn" id="edit-btn" data-id="expense-${expense.id}">
+                                <i class="fas fa-pencil-alt"></i>
+                            </button>
+                        </li>
+                    `).join('');
+                }
+            }
+        } else {
+            console.error('Erro ao obter as transações do usuário');
+        }
+    } catch (error) {
+        console.error('Erro ao enviar a requisição');
+    }
+
+    // When the burger button is clicked, activates the hidden nav menu
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+    });
+
+    document.querySelectorAll('.delete-btn').forEach(button => {
+        button.addEventListener('click', async (event) => {
+            const id = event.target.getAttribute('data-id').split('-')[1];
+            const endpoint = event.target.getAttribute('data-id').split('-')[0];
+            try {
+                const response = await fetch(`${API_URL}/${endpoint}s/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    },
+                });
+
+                if (response.ok) {
+                    window.location.reload();
+                } else {
+                    console.error('Erro ao excluir o item');
+                }
+            } catch (error) {
+                console.error('Erro ao enviar a requisição de exclusão');
+            }
+        });
+    });
+
+    document.querySelectorAll('.delete-btn-desktop').forEach(button => {
+        button.addEventListener('click', async (event) => {
+            const id = event.target.getAttribute('data-id').split('-')[1];
+            const endpoint = event.target.getAttribute('data-id').split('-')[0];
+            try {
+                const response = await fetch(`${API_URL}/${endpoint}s/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    },
+                });
+
+                if (response.ok) {
+                    window.location.reload();
+                } else {
+                    console.error('Erro ao excluir o item');
+                }
+            } catch (error) {
+                console.error('Erro ao enviar a requisição de exclusão');
+            }
+        });
+    });
+
+    document.getElementById('view-all-expenses-btn').addEventListener('click', () => {
+        window.location.href = 'expenses-dashboard.html'; 
+    });
+
+    document.getElementById('view-all-incomes-btn').addEventListener('click', () => {
+        window.location.href = 'incomes-dashboard.html'; 
+    });
+});
